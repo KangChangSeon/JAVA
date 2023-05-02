42ab6736b27387a320fa76f5188776d6ff64f9e9
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2023-05-02 12:43:15 +0900

    Update test6.java

--- Ch05/src/test6.java
+++ Ch05/src/test6.java
@@ -20,3 +20,9 @@ public class test6
         //equals()는 문자열의 내용이 같은지 다른지 확인하는데 사용한다. 대소문자를 구분한다는 점을 주의해야한다.//
     }
 }
+ 
+    // char charAt(int Index) = 문자열에서 해당 위치(Index)에 있는 문자를 반환한다.
+    // int length() = 문자열의 길이를 반환한다.
+    // String substring(int from, int to) = 문자열에서 해당 범위(from~to)의 문자열을 반환한다.(to는 포함 안됨)
+    // boolean equals(Object obj) = 문자열의 내용이 같은지 확인한다. 같으면 결과는 true, 다르면 false
+    // char[] toCharArray() = 문자열을 문자배열(char[])로 변환해서 반환한다.
